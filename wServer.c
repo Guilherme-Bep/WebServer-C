3525987f4eda86c5b8c7ad6f3b1ded0f3d80df13
Guilherme Bep <Guilherme-Bep@users.noreply.github.com>
2025-04-04 22:43:43 -0300

    Pipeline
    Descrição do projeto e elaboração da estrutura básica

---
+++ wServer.c
@@ -0,0 +1,17 @@
+// Web server para hospedar aplicação em html, css e javascript
+
+//Estrutura básica do servidor
+
+/*
+ - Criar as variáveis
+ - Inicializar as variáveis
+ - Criar o socket
+ - Abrir o socket
+ - Criar o bind
+ - Bind do socket com a interface de rede
+ - Receber conexões
+ - loop para conexões
+
+*/
+
+
